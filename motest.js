045a8ac129deb3043534a6e7bfb5965d2f6cb756
Tibirius <tibirius@gmail.com>
2019-01-26 21:25:32 -0500

    Cleanup

--- motest.js
+++
@@ -1,6 +0,0 @@
-var moment = require('moment');
-var tee ="2019-01-26T20:00"
-var ime = moment(tee).format("MM/DD/YYYY")
-console.log(ime)
-var timeNow=moment().format()
-console.log(moment().format());
